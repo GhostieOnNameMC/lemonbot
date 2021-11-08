e132bfc35379ed8061c0e297fba4202378bd09aa
duck9k <bradleyflexual@gmail.com>
2021-11-08 16:02:06 +0000

    Fixed Prefixes being incoreect for the command names

--- commands/cmds.js
+++ commands/cmds.js
@@ -7,14 +7,14 @@ module.exports = {
         .setColor('#0099ff')
         .setTitle('Commands')
         .addFields(
-            { name: '+aiimage', value: 'Gives you an ai generated image based of text.'},
-            { name: '+fact', value: 'Gives you some facts.'},
-            { name: '+help', value: 'Gives you this prompt.'},
-            { name: '+invite', value: 'Gives you the invite link for the bot.'},
-            { name: '+play', value: 'Plays Music'},
-            { name: '+skip', value: 'Skips currently playing music'},
-            { name: '+stop', value: 'Disconnects the bot from vc'},
-            { name: '+wallpaper', value: 'Gives you a wallpaper'}
+            { name: '!aiimage', value: 'Gives you an ai generated image based of text.'},
+            { name: '!fact', value: 'Gives you some facts.'},
+            { name: '!help', value: 'Gives you this prompt.'},
+            { name: '!invite', value: 'Gives you the invite link for the bot.'},
+            { name: '!play', value: 'Plays Music'},
+            { name: '!skip', value: 'Skips currently playing music'},
+            { name: '!stop', value: 'Disconnects the bot from vc'},
+            { name: '!wallpaper', value: 'Gives you a wallpaper'}
         )
         message.channel.send(embed);
     }
